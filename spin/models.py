fcb72eade7b0eb87a202611e6e34e05187a6bfee
DominikZabron <dominik.zabron@gmail.com>
2016-08-24 13:09:26 +0200

    Add wallets to models.

--- spin/models.py
+++ spin/models.py
@@ -3,7 +3,9 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, DECIMAL, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy_utils import UUIDType
 from flask_login import UserMixin
 
@@ -13,9 +15,14 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(UUIDType, primary_key=True, default=uuid.uuid4)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(255))
+    __tablename__ = 'user'
+    id = Column(UUIDType, primary_key=True, default=uuid.uuid4)
+    username = Column(String(80), unique=True)
+    password = Column(String(255))
+    eur_account = relationship(
+        "EurAccount", uselist=False, back_populates="user")
+    bns_account = relationship(
+        "BnsAccount", uselist=False, back_populates="user")
 
     def __init__(self, username, password):
         self.username = username
@@ -30,3 +37,23 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return '<User {0}>'.format(self.username)
+
+
+class WalletMixin(object):
+    id = Column(Integer, primary_key=True)
+    balance = Column(DECIMAL(12, 2))
+
+    @declared_attr
+    def user_id(cls):
+        return Column(UUIDType, ForeignKey('user.id'))
+
+
+class EurAccount(db.Model, WalletMixin):
+    __tablename__ = 'eur_account'
+    user = relationship("User", back_populates="eur_account")
+
+
+class BnsAccount(db.Model, WalletMixin):
+    __tablename__ = 'bns_account'
+    user = relationship("User", back_populates="bns_account")
+
